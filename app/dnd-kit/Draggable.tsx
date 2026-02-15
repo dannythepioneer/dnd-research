75369499a9de4ff59b8a5a7f5f80ee01029fbf0c
Danny Chen <reach.us@dannychen.pro>
2026-02-15 18:11:58 +0800

    Refamiliarizing with some Tailwind CSS

--- app/dnd-kit/Draggable.tsx
+++ app/dnd-kit/Draggable.tsx
@@ -5,7 +5,7 @@ export function Draggable({id}:{id: string}) {
 	const {ref} = useDraggable({id});
 	
 	return (
-		<button ref={ref}>
+		<button ref={ref} className="btn btn-primary btn-lg btn-block shadow-sm px-2.5 py-1.5 bg-blue-500 rounded-md text-white">
 			Draggable
 		</button>
 	);
